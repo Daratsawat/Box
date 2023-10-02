fd1e40a69dd3ec1502498f8251705b18ff538f54
DARATSAWAT THIENSAWAT <daratsawat.thie@bu.ac.th>
2023-10-03 05:11:04 +0700

    Complete Project

---
+++ Pages/Siam/IndexSiam.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace Box.Pages.Siam
+{
+    public class IndexSiamModel : PageModel
+    {
+        public List<StockInfo> liststocks = new List<StockInfo>();
+
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Server=tcp:box094207.database.windows.net,1433;Initial Catalog=BoxSystem;Persist Security Info=False;User ID=Daratsawat;Password=Icetea094207;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM stocks WHERE storeid=2";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                StockInfo stockInfo = new StockInfo();
+                                stockInfo.itemid = "" + reader.GetInt32(0);
+                                stockInfo.item = reader.GetString(1);
+                                stockInfo.storeid = reader.GetString(2);
+                                stockInfo.supplier = reader.GetString(3);
+                                stockInfo.amount = reader.GetString(4);
+                                stockInfo.create_at = reader.GetDateTime(5).ToString();
+
+                                liststocks.Add(stockInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+
+    public class StockInfo
+    {
+        public String itemid;
+        public String item;
+        public String storeid;
+        public String supplier;
+        public String amount;
+        public String create_at;
+    }
+
+
+
+}
